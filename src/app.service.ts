500898a31861e69e1be5974762fab7b1b3e2838d
mj-song00 <niveus128@gmail.com>
2023-08-17 14:39:01 +0900

    Chore: user 모듈 완성

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'we are chips!';
+    return '너는 또 왜 안되는건데!!1';
   }
 }
--- src/user/dto/create-user.dto.ts
+++ src/user/dto/create-user.dto.ts
@@ -10,7 +10,7 @@ export class CreateUserDto {
   @IsString()
   @Matches(/^(?=.*[a-z\d])(?=.*[!@#$%^&*()])[a-z\d!@#$%^&*()]{8,12}$/, {
     message:
-      '비밀번호는 소문자, 특수문자 및 숫자를 모두 포함한 8자리 이상 12자리 이하여야 합니다.',
+      '비밀번호는 소문자, 특수문자 및 숫자를 모두 포함한 8자리 이상이여야 합니다.',
   })
   password: string;
 
@@ -18,4 +18,15 @@ export class CreateUserDto {
   @IsString()
   @Match('password', { message: '비밀번호가 일치하지 않습니다.' })
   confirmPassword: string;
+}
+
+export class SignInDto {
+  @IsNotEmpty()
+  @IsEmail()
+  @IsString()
+  email: string;
+
+  @IsNotEmpty()
+  @IsString()
+  password: string;
 }
