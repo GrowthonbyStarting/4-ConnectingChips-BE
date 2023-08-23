02e1c753a322ff3b84123fdeafb3a3cbb2d6611e
mj-song00 <niveus128@gmail.com>
2023-08-23 13:00:09 +0900

    오류수정

--- src/project/project.service.ts
+++ src/project/project.service.ts
@@ -2,6 +2,7 @@ import { ImagesService } from './../image/images.service';
 import { PrismaService } from './../../prisma/prisma.service';
 import { Injectable } from '@nestjs/common';
 import { CreateProjectDto } from './dto/create-project.dto';
+import { targetModulesByContainer } from '@nestjs/core/router/router-module';
 
 @Injectable()
 export class ProjectService {
@@ -11,15 +12,23 @@ export class ProjectService {
   ) {}
   async create(createProjectDto: CreateProjectDto, file: Express.Multer.File) {
     const { title, type, roles, contents } = createProjectDto;
-    const projectImage = await this.imagesService.create(file);
+    console.log(title, type, roles, contents);
+    // const projectImage = await this.imagesService.create(file);
 
-    const project = this.prisma.project.create({
-      title,
-      tabs: type,
-      roles,
-      contents,
-      image: { connect: { id: projectImage.id } },
-    });
+    // const projectTabs = {
+    //   create: type.localeCompare()
+    // }
+    // const project = await this.prisma.project.create({
+    //   data: {
+    //     title,
+    //     tabs,
+    //     rolse,
+    //     contents,
+    //   },
+    // });
+    //   //image: { connect: { id: projectImage.id } },
+    // console.log(project);
+    // return { result: `작심 생성 완료` };
   }
 
   // findAll() {
