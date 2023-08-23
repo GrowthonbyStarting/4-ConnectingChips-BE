2ae08c95c848db539dfc8dd9f3329dea5171a1b1
mj-song00 <niveus128@gmail.com>
2023-08-23 16:58:26 +0900

    Chore: s3연결 해제 및 파일,폴더 수정

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,13 @@
-import { GruopModule } from './project/gruop.module';
+import { GruopModule } from './group/gruop.module';
 import { AdminModule } from './account/admin/admin.module';
 import { ImagesModule } from './image/images.module';
 import { S3Module } from './s3/s3.module';
 import { PrismaModule } from './../prisma/prisma.module';
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './account/user/user.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
